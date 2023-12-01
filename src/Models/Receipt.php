89e0bc47a63bf0eff11735dddda17586716d34de
Zsolt148 <Zsolt148@users.noreply.github.com>
2023-12-01 14:30:34 +0000

    Fix styling

--- src/Models/Receipt.php
+++ src/Models/Receipt.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Illuminate\Support\Facades\Storage;
+
 /**
  * @property int $id
  * @property string $model_type
